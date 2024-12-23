c2ad6b33762ade420437c63ec2724e34ae6e680d
Dmitry Sokun <Dmitry.Sokun@miningelement.com>
2024-12-23 20:08:39 +0300

    Копия

---
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###
+# Инициализируем бд
+###
+
+docker compose exec -T router mongosh --port 27017 <<EOF
+use somedb;
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
+EOF
+
