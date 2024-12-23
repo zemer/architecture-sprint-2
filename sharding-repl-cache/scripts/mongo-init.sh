9f09ed5b9b163e9641d58a9ff1308bf8da06a132
Dmitry Sokun <Dmitry.Sokun@miningelement.com>
2024-12-23 21:18:10 +0300

    Добавлен redis

--- sharding-repl-cache/scripts/mongo-init.sh
+++ sharding-repl-cache/scripts/mongo-init.sh
@@ -72,3 +72,7 @@ for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
 exit();
 EOF
 
+docker compose exec -T redis_1 sh <<EOF
+echo "yes" | redis-cli --cluster create redis_1:6379 redis_2:6379 redis_3:6379 redis_4:6379 redis_5:6379 redis_6:6379 --cluster-replicas 1
+EOF
+
